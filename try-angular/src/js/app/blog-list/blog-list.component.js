fd31c5a59aa94f636d35c85539c6e7b46ee9774e
SOunit <workbookwork7@gmail.com>
2022-09-07 14:45:52 -0700

    add memo

--- try-angular/src/js/app/blog-list/blog-list.component.js
+++ try-angular/src/js/app/blog-list/blog-list.component.js
@@ -6,12 +6,10 @@ angular.module('blogList').component('blogList', {
     <button ng-click="someClickTest()">Click me!</button>
   </div>`,
   controller: function ($scope) {
-    console.log('Hello');
     $scope.title = 'Hi, there';
     $scope.clicks = 0;
 
     $scope.someClickTest = function () {
-      console.log('click test!');
       $scope.clicks += 1;
       $scope.title = `Clicked! ${$scope.clicks}`;
     };
