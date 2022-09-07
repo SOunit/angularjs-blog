2ecb1f1b91c621fb90b83753888fab627c7bfb0f
SOunit <workbookwork7@gmail.com>
2022-09-07 15:24:16 -0700

    add sample

--- try-angular/src/js/app/blog-list/blog-list.component.js
+++ try-angular/src/js/app/blog-list/blog-list.component.js
@@ -7,6 +7,21 @@ angular.module('blogList').component('blogList', {
   // </div>`,
   templateUrl: '/try-angular/src/templates/blog-list.html',
   controller: function ($scope) {
+    var blogItems = [
+      {
+        title: 'Some Title1',
+        id: 1,
+        description: 'this is a book1',
+        publishDate: '2016-07-11',
+      },
+      { title: 'Some Title2', id: 2, description: 'this is a book2' },
+      { title: 'Some Title3', id: 3, description: 'this is a book3' },
+      { title: 'Some Title4', id: 4, description: 'this is a book4' },
+      { title: 'Some Title5', id: 5, description: 'this is a book5' },
+    ];
+
+    $scope.items = blogItems;
+
     $scope.title = 'Hi, there';
     $scope.clicks = 0;
 
