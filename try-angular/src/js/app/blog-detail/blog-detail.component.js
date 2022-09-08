3ed28454a3ade3c66dc9c5298ca583c51bccc381
SOunit <workbookwork7@gmail.com>
2022-09-08 00:00:09 -0700

    add http request

--- try-angular/src/js/app/blog-detail/blog-detail.component.js
+++ try-angular/src/js/app/blog-detail/blog-detail.component.js
@@ -2,33 +2,45 @@
 
 angular.module('blogDetail').component('blogDetail', {
   templateUrl: 'templates/blog-detail.html',
-  controller: function ($location, $routeParams, $scope) {
-    var blogItems = [
-      {
-        title: 'Some Title1',
-        id: 1,
-        description: 'this is a book1',
-        publishDate: '2016-07-11',
-      },
-      { title: 'Some Title2', id: 2, description: 'this is a book2' },
-      { title: 'Some Title3', id: 3, description: 'this is a book3' },
-      { title: 'Some Title4', id: 4, description: 'this is a book4' },
-      { title: 'Some Title5', id: 5, description: 'this is a book5' },
-    ];
+  controller: function ($http, $location, $routeParams, $scope) {
+    // var blogItems = [
+    //   {
+    //     title: 'Some Title1',
+    //     id: 1,
+    //     description: 'this is a book1',
+    //     publishDate: '2016-07-11',
+    //   },
+    //   { title: 'Some Title2', id: 2, description: 'this is a book2' },
+    //   { title: 'Some Title3', id: 3, description: 'this is a book3' },
+    //   { title: 'Some Title4', id: 4, description: 'this is a book4' },
+    //   { title: 'Some Title5', id: 5, description: 'this is a book5' },
+    // ];
 
-    $scope.title = `Blog ${$routeParams.id}`;
     $scope.notFound = true;
 
-    blogItems.forEach((post) => {
-      if (post.id === +$routeParams.id) {
-        $scope.post = post;
-        $scope.notFound = false;
-      }
-    });
+    $http
+      .get('/try-angular/src/json/posts.json')
+      .then(successCallback, errorCallback);
 
-    if ($scope.notFound) {
-      console.log('Blog Post Not Found');
-      $location.path('/404');
+    function successCallback(response, status, config, statusText) {
+      console.log(response.data);
+      $scope.posts = response.data;
+
+      $scope.posts.forEach((post) => {
+        if (post.id === +$routeParams.id) {
+          $scope.post = post;
+          $scope.notFound = false;
+        }
+      });
+
+      if ($scope.notFound) {
+        console.log('Blog Post Not Found');
+        $location.path('/404');
+      }
+    }
+
+    function errorCallback(response, status, config, statusText) {
+      console.log(response);
     }
   },
 });
