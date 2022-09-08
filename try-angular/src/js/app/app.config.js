c066acb8914aac399b361cf765f9822d1a89d232
SOunit <workbookwork7@gmail.com>
2022-09-07 17:36:28 -0700

    add route

--- try-angular/src/js/app/app.config.js
+++ try-angular/src/js/app/app.config.js
@@ -1,3 +1,22 @@
 'use strict';
 
-angular.module('try').config(function () {});
+angular.module('try').config(function ($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+
+  // FIXME: not working
+  //   $locationProvider.html5Mode({ enabled: true });
+
+  $routeProvider
+    .when('/', {
+      template: '<blog-list></blog-list>',
+    })
+    .when('/blog/1', {
+      template: '<h1>Hi</h1>',
+    })
+    .when('/blog/2', {
+      template: '<blog-list></blog-list>',
+    })
+    .otherwise({
+      template: 'Not found',
+    });
+});
